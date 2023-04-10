4ea7134dfc6e987f2819e09b50774c6e26e9e78e
Aaron Höper <ahoeper@united-internet.de>
2023-04-10 18:29:42 +0200

    Sorting---

--- src/main/java/pruefung/Main.java
+++ src/main/java/pruefung/Main.java
@@ -10,11 +10,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Raum[] raueme = new Raum[20];
-        for (int i = 0; i < raueme.length; i++) {
-            raueme[i] = new Raum(random());
-        }
-        sort(raueme);
+        Raum[] raueme = {new Raum(2), new Raum(1), new Raum(5), new Raum(4), new Raum(6), new Raum(3)};
+        //sort(raueme);
+        sortDescending(raueme);
     }
 
     public static void reverseArray() {
@@ -29,12 +27,57 @@ public class Main {
 
     public static void sort(Raum[] raueme) {
         StringBuilder sb = new StringBuilder();
+        sb.append("Unsortiert: ");
         Arrays.stream(raueme).forEach(raum -> sb.append(raum.getBelegung()).append(", "));
         System.out.println(sb);
-        for()
+        /*
+        ---------------- Eigentliche Aufgabe ----------------
+         */
+        int counter = 0;
+        for (int i = 0; i < raueme.length - 1; i++) {
+            for (int j = 0; j < raueme.length - i - 1; j++) {
+                counter++;
+                if (raueme[j].getBelegung() > raueme[j + 1].getBelegung()) {
+                    Raum raum = raueme[j];
+                    raueme[j] = raueme[j + 1];
+                    raueme[j + 1] = raum;
+                }
+                sb.setLength(0);
+                sb.append(counter).append(". Nach der Änderung: ");
+                Arrays.stream(raueme).forEach(raum -> sb.append(raum.getBelegung()).append(", "));
+                System.out.println(sb);
+            }
+        }
+        /*
+        ---------------- Eigentliche Aufgabe ----------------
+         */
         System.out.println("----------------");
         sb.setLength(0);
+        sb.append("Sortiert: ");
         Arrays.stream(raueme).forEach(raum -> sb.append(raum.getBelegung()).append(", "));
+        sb.append(" (").append(counter).append(" Iterationen)");
+        System.out.println(sb);
+    }
+
+    public static void sortDescending(Raum[] raueme) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Unsortiert: ");
+        Arrays.stream(raueme).forEach(raum -> sb.append(raum.getBelegung()).append(", "));
+        System.out.println(sb);
+        for (int i = 0; i < raueme.length - 1; i++) {
+            for (int j = 0; j < raueme.length - i - 1; i++) {
+                if (raueme[j].getBelegung() > raueme[j + 1].getBelegung()) {
+                    Raum raum = raueme[j];
+                    raueme[j + 1] = raueme[j];
+                    raueme[j] = raum;
+                }
+            }
+        }
+        System.out.println("----------------");
+        sb.setLength(0);
+        sb.append("Sortiert: ");
+        Arrays.stream(raueme).forEach(raum -> sb.append(raum.getBelegung()).append(", "));
+        sb.append(" (").append(" Iterationen)");
         System.out.println(sb);
     }
 }
